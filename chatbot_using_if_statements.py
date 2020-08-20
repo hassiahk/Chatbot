516eb70665eae60eb517d9a2640135f4c015c77d
Haswanth Kumar Aekula <haswanth.kumar.39@gmail.com>
2020-08-20 18:12:50 +0530

    Add files via upload

---
+++ chatbot_using_if_statements.py
@@ -0,0 +1,16 @@
+import os
+
+while True:
+    query = input('Can you please tell me what can I do for you? ')
+    if 'run' in query or 'open' in query:
+        if 'notepad' in query or 'edit' in query:
+            os.system('notepad')
+        if 'chrome' in query or 'browser' in query:
+            os.system('start chrome')
+        if 'media' in query or 'player' in query:
+            os.chdir('C:\Program Files\VideoLAN\VLC')
+            os.system('vlc')
+    elif 'exit' in query or 'terminate' in query or 'stop' in query:
+        break
+    else:
+        print('Can you please try again')
